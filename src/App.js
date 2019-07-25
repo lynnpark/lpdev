e24b37d0bdd5799cf532c26e49c7735176ba10a3
Lynn <lynn.park@optum.com>
2019-07-25 19:38:38 -0400

    width

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
 const ContentWrapper = styled.div`
   margin-left: auto;
   margin-right: auto;
+  width: calc(100% - 32px);
   max-width: 42rem;
   padding: 2.625rem 1.3125rem;
 `;
